01b52dbbba899f33a9b2aae5c670be30c92d20ac
jenifer805 <jenifer.huanca@ucsm.edu.pe>
2026-05-10 20:38:19 -0500

    Create ItemDuplicated.java

---
+++ ItemDuplicated.java
@@ -0,0 +1,9 @@
+package Exceptions;
+public class ItemDuplicated extends Exception {
+    public ItemDuplicated(String msg) {
+        super(msg);
+    }
+    public ItemDuplicated() {
+        super();
+    }
+}
