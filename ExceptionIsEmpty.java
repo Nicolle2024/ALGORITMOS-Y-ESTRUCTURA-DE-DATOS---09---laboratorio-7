ccc3a93bc872501527ece56d8e28e0d4829b0816
jenifer805 <jenifer.huanca@ucsm.edu.pe>
2026-05-10 20:37:30 -0500

    Create ExceptionIsEmpty.java

---
+++ ExceptionIsEmpty.java
@@ -0,0 +1,9 @@
+package Exceptions;
+public class ExceptionIsEmpty extends Exception {
+    public ExceptionIsEmpty(String msg) {
+        super(msg);
+    }
+    public ExceptionIsEmpty() {
+        super();
+    }
+}
